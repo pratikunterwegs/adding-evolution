8fce64da3fbeb8a5da3ac0b010a8a4d9a9784ca9
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-10 16:01:21 +0200

    add simulation evo

--- src/simulations.cpp
+++ src/simulations.cpp
@@ -97,6 +97,87 @@ using namespace Rcpp;
 //     );
 // }
 
+/// simulation for evolved probability of switching to search
+Rcpp::List simulation::do_simulation_evo() {
+    unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
+    rng.seed(seed);
+    
+    // prepare landscape and pop
+    food.initResources();
+    food.countAvailable();
+    Rcpp::Rcout << "landscape with " << food.nClusters << " clusters\n";
+
+    pop.setTrait();
+    Rcpp::Rcout << "pop with " << pop.nAgents << " agents for " << genmax << " gens " << tmax << " timesteps\n";
+
+    // prepare scenario
+    // return scenario as string
+    Rcpp::Rcout << "this is scenario " << scenario << ": evolutionary\n";
+
+    // agent random position in first gen
+    pop.initPos(food);
+
+    Rcpp::Rcout << "initialised population positions\n";
+    Rcpp::DataFrame edgeList;
+    Rcpp::DataFrame pop_trait_data;
+
+    Rcpp::Rcout << "created single edge list object\n";
+
+    // all ecological dynamics
+    food.countAvailable();
+    // reset counter and positions
+    pop.counter = std::vector<int> (pop.nAgents, 0);
+
+    // go over gens
+    for(int gen = 0; gen < genmax; gen ++) {
+        food.countAvailable();
+        pop.counter = std::vector<int> (pop.nAgents, 0);
+
+        // timesteps start here
+        for (size_t t = 0; t < static_cast<size_t>(tmax); t++)
+        {
+            // resources regrow
+            food.regenerate();
+            pop.updateRtree();
+            // movement section
+            pop.move_random(food);
+
+            md.updateMoveData(pop, t);
+
+            // foraging -- split into parallelised picking
+            // and non-parallel exploitation
+            pop.pickForageItem(food, nThreads);
+            pop.doForage(food);
+
+            // count associations
+            pop.countAssoc(nThreads);
+            // timestep ends here
+        }
+
+        // log data in the last generation
+        if (gen == (genmax - 1)) {
+            pop_trait_data = pop.returnPopData();
+            edgeList = pop.pbsn.getNtwkDf();
+        }
+
+    }
+
+    // generation ends here
+    Rcpp::Rcout << "gen: " << (genmax - 1) << " --- logged edgelist\n";
+    Rcpp::Rcout << "data prepared\n";
+
+    edgeList = pop.pbsn.getNtwkDf();
+
+    Rcpp::Rcout << "gen: " << 1 << " --- logged edgelist\n";
+    Rcpp::Rcout << "data prepared\n";
+
+    return Rcpp::List::create(
+        Named("gen_data") = pop.returnPopData(),
+        Named("edge_list") = edgeList,
+        Named("move_data") = md.getMoveData()
+    );
+}
+
 /// simulation for random movement
 Rcpp::List simulation::do_simulation_eco() {
     unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
@@ -147,7 +228,6 @@ Rcpp::List simulation::do_simulation_eco() {
         pop.countAssoc(nThreads);
         // timestep ends here
     }
-    pop.energy = pop.intake;
     
     edgeList = pop.pbsn.getNtwkDf();
 
--- src/simulations.h
+++ src/simulations.h
@@ -83,6 +83,7 @@ public:
 
     // funs
     Rcpp::List do_simulation_eco();
+    Rcpp::List do_simulation_evo();
 
 };
 
