e54cea44df6d7b637a8793e42c98947260f1380d
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-10 10:18:21 +0200

    no gsl

--- src/parameters.h
+++ src/parameters.h
@@ -5,16 +5,10 @@
 // [[Rcpp::plugins("cpp14")]]
 // [[Rcpp::depends(BH)]]
 // [[Rcpp::depends(RcppParallel)]]
-// [[Rcpp::depends(RcppGSL)]]
 #include <random>
-#include <RcppGSL.h>
 #include <chrono>
 
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
-
 extern std::mt19937 rng;
-extern gsl_rng *r;
 
 // landscape
 const double foodEnergy = 1.0;
