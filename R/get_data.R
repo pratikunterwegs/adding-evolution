b7a712f905100c0acd8fa8d7b2e4b7a52e6134e9
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-30 16:31:50 +0200

    fun to get trait data

--- R/get_data.R
+++ R/get_data.R
@@ -13,3 +13,23 @@ get_move_data = function(object) {
 
     move_data
 }
+
+#' Get trait data.
+#'
+#' @param object A `simulation_output` object.
+#'
+#' @return A `data.table` object.
+#' @export
+#'
+get_trait_data = function(object) {
+    trait_data = object@trait_data
+    trait_data = Map(
+        trait_data, seq(length(trait_data)),
+        f = function(df, g) {
+            df$gen = g
+            df
+        }
+    )
+    trait_data = data.table::rbindlist(trait_data)
+    trait_data
+}
